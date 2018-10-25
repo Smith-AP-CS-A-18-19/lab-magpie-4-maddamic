9f5b3505d85e6c2cc9835992d259dd4dd5943d35
Michael Maddalongit config --global user.email maddamic@berkeleyprep.org <you@example.com>
2018-10-25 14:58:34 -0400

    Commit

--- Magpie4.java
+++ Magpie4.java
@@ -10,6 +10,11 @@
  * @version April 2012
  *
  */
+
+
+	// Michael Maddalon
+
+
 public class Magpie4 {
 	/**
 	 * Get a default greeting
@@ -76,6 +81,21 @@ public class Magpie4 {
 		int psn = findKeyword (statement, "I want to", 0);
 		String restOfStatement = statement.substring(psn + 9).trim();
 		return "What would it mean to " + restOfStatement + "?";
+	}
+
+
+	private String transformIDontWantToStatement(String statement) {
+		//  Remove the final period, if there is one
+		statement = statement.trim();
+		String lastChar = statement.substring(statement
+				.length() - 1);
+		if (lastChar.equals(".")) {
+			statement = statement.substring(0,
+							statement.length() - 1);
+		}
+		int psn = findKeyword (statement, "I don't want to", 0);
+		String restOfStatement = statement.substring(psn + 15).trim();
+		return "Why don't you want to " + restOfStatement + "?";
 	}
 
 
